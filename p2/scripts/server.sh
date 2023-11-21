d86f297a0f24a5f10b7b45091a93191caa421fe7
laballea <aballea.louis@gmail.com>
2023-11-21 11:01:39 -0500

    ok

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -1,4 +1,4 @@
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1 --bind-address=$1 --advertise-address=$1" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface eth1 --write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1 --bind-address=$1 --advertise-address=$1" sh -
 
 echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
 
